c54fd316d06b88127b2834196bbf7004690a0e1d
Chris Uehlinger <chris.uehlinger@gmail.com>
2017-04-12 02:38:00 +0000

    Improve share-leap script

--- share-leap.sh
+++ share-leap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo killall ftusbnetd;
+sudo /opt/ftusbnet/sbin/ftusbnetd -d
 LEAP_USB_ID=`/opt/ftusbnet/bin/ftusbnetctl list | grep "Leap Motion Controller" | awk '{ print $1 }'`
 sudo /opt/ftusbnet/bin/ftusbnetctl share $LEAP_USB_ID
