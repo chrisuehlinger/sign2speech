10c651269fb4458cfadb944813fba4a7f7a646f4
Chris Uehlinger <chris.uehlinger@gmail.com>
2017-04-14 22:42:32 -0400

    Add gulp workflow for starting web server

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/speak', function(req,res){
   res.sendStatus(200);
 });
 
-app.listen(8585, function(){
-  console.log('app listening on port 8585!');
+app.listen(5000, function(){
+  console.log('app listening on port 5000!');
 });
